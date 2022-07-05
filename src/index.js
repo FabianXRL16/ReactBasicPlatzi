0b337ee110f072e2363c0af1e94fca20325f30bb
FabianXRL16 <fabianpacherres@gmail.com>
2022-07-05 12:49:51 -0500

    react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { App } from './App'
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
