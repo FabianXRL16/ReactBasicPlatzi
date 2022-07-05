49d0ee7a9e0cfa0cfa08358a6c3de92c12cdede3
FabianXRL16 <fabianpacherres@gmail.com>
2022-07-05 12:49:26 -0500

    app js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import './App.css';
-
 import TodoCounter from './components/TodoCounter/TodoCounter'
 import TodoSearch from './components/TodoSearch/TodoSearch'
 import TodoList from './components/TodoList/TodoList'
 import CreateTodoButton from './components/CreateTodoButton/CreateTodoButton'
+import CreateItemButton from './components/CreateItemButton/CreateItemButton'
 
-function App() {
+import React, { useState } from 'react'
+
+export function App() {
+  const [todos, setTodos] = useState([])
+  
+  const newItem = (task) => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, task ]
+    })
+  }
+
+
   return (
     <div className="App">
       <TodoCounter />
       <TodoSearch />
-      <TodoList />
+      <CreateItemButton newItem={newItem} />
+      <TodoList todos={ todos } />
       <CreateTodoButton />
     </div>
   );
 }
-
-export default App;
