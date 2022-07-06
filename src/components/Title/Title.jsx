f81bea527ab5ec5d90ff1cbac9ccd52364dcd90c
FabianXRL16 <fabianpacherres@gmail.com>
2022-07-05 23:56:13 -0500

    Create component Title

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Title.css'
+
+export default function Title({title}) {
+  return (
+    <div className='Title'>
+        <span>{title}</span>
+    </div>
+  )
+}
